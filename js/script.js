04cd16275c9c3140c7d911a2249337ef783a79f1
Aakash <aakashtiwari605@gmail.com>
2022-07-05 22:25:50 +0530

    Add

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// =============================Typing animation===========
+var typed = new Typed(".typing",{
+    String:["Web Designer","Web Developer","Graphic Designer","Youtuber"],
+    typespeed:100,
+    BackSpeed:60,
+    loop:true
+})
